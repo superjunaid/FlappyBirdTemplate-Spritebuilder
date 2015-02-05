60ff1d19287e7b0c0deda4f4ae4f8aa1aead0b91
Syed Junaid Ahmed <syedjahmed@usdoj.gov>
2015-02-05 18:02:17 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -12,11 +12,22 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle > 2.0f) {
+        
+        [self addObstacle];
+        
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
