45d4c5ec384b68885457cb3a7c81a70c3df16903
Syed Junaid Ahmed <syedjahmed@usdoj.gov>
2015-02-09 12:29:42 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -165,51 +165,6 @@
 
 - (void)update:(CCTime)delta
 {
-    _sinceTouch += delta;
-    
-    character.rotation = clampf(character.rotation, -30.f, 90.f);
-    
-    if (character.physicsBody.allowsRotation) {
-        float angularVelocity = clampf(character.physicsBody.angularVelocity, -2.f, 1.f);
-        character.physicsBody.angularVelocity = angularVelocity;
-    }
-    
-    if ((_sinceTouch > 0.5f)) {
-        [character.physicsBody applyAngularImpulse:-40000.f*delta];
-    }
-    
-    physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
-    
-    
-    // move and loop the bushes
-    for (CCNode *bush in _bushes) {
-        // move the bush
-        bush.position = ccp(bush.position.x -
-                            (character.physicsBody.velocity.x * delta), bush.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (bush.position.x <= (-1 * bush.contentSize.width)) {
-            bush.position = ccp(bush.position.x +
-                                2 * bush.contentSize.width, bush.position.y);
-        }
-    }
-    
-    // move and loop the clouds
-    for (CCNode *cloud in _clouds) {
-        // move the cloud
-        cloud.position = ccp(cloud.position.x -
-                             (character.physicsBody.velocity.x * delta), cloud.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
-            cloud.position = ccp(cloud.position.x +
-                                 2 * cloud.contentSize.width, cloud.position.y);
-        }
-    }
-    
-    /*
     // loop the bushes
     for (CCNode * bush in _bushes) {
         //get the world position of the bush
@@ -247,7 +202,55 @@
             }
         }
     }
-     */
+    
+    _sinceTouch += delta;
+    
+    character.rotation = clampf(character.rotation, -30.f, 90.f);
+    
+    if (character.physicsBody.allowsRotation) {
+        float angularVelocity = clampf(character.physicsBody.angularVelocity, -2.f, 1.f);
+        character.physicsBody.angularVelocity = angularVelocity;
+    }
+    
+    if ((_sinceTouch > 0.5f)) {
+        [character.physicsBody applyAngularImpulse:-40000.f*delta];
+    }
+    
+    physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
+    
+    /*
+    
+    // move and loop the bushes
+    for (CCNode *bush in _bushes) {
+        // move the bush
+        bush.position = ccp(bush.position.x -
+                            (character.physicsBody.velocity.x * delta), bush.position.y);
+        
+        // if the left corner is one complete width off the screen,
+        // move it to the right
+        if (bush.position.x <= (-1 * bush.contentSize.width)) {
+            bush.position = ccp(bush.position.x +
+                                2 * bush.contentSize.width, bush.position.y);
+        }
+    }
+    
+    // move and loop the clouds
+    for (CCNode *cloud in _clouds) {
+        // move the cloud
+        cloud.position = ccp(cloud.position.x -
+                             (character.physicsBody.velocity.x * delta), cloud.position.y);
+        
+        // if the left corner is one complete width off the screen,
+        // move it to the right
+        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+            cloud.position = ccp(cloud.position.x +
+                                 2 * cloud.contentSize.width, cloud.position.y);
+        }
+    }
+    */
+    
+   
+    
     
     // loop the ground
     for (CCNode *ground in _grounds) {
