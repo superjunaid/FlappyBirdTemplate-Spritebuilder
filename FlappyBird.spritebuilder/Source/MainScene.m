85e6b141499070e9c775a9c80b377c301ba94bbb
Syed Junaid Ahmed <syedjahmed@usdoj.gov>
2015-02-09 15:20:18 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -167,6 +167,10 @@
 
 - (void)update:(CCTime)delta
 {
+    
+    _parallaxBackground.position = ccp(_parallaxBackground.position.x - (character.physicsBody.velocity.x * delta), _parallaxBackground.position.y);
+    
+
     // loop the bushes
     for (CCNode * bush in _bushes) {
         //get the world position of the bush
