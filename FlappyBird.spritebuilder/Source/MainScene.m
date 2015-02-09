794104a30742a9eba92da0c11ca8c0661823a01d
Syed Junaid Ahmed <syedjahmed@usdoj.gov>
2015-02-09 15:57:50 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -58,13 +58,14 @@
 
 - (void)didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
-    
-    _parallaxBackground = [CCParallaxNode node];
-    [_parallaxContainer addChild:_parallaxBackground];
-    
+
     _grounds = @[_ground1, _ground2];
     _clouds = @[_cloud1, _cloud2];
     _bushes = @[_bush1, _bush2];
+    
+   // _parallaxBackground = [CCParallaxNode node];
+   // [_parallaxContainer addChild:_parallaxBackground];
+    
     
     //Note that the bush ratio is larger than the cloud
     _bushParallaxRatio = ccp(0.9, 1);
@@ -223,36 +224,7 @@
     physicsNode.position = ccp(physicsNode.position.x - (character.physicsBody.velocity.x * delta), physicsNode.position.y);
     
     
-    
-    // move and loop the bushes
-    for (CCNode *bush in _bushes) {
-        // move the bush
-        bush.position = ccp(bush.position.x -
-                            (character.physicsBody.velocity.x * delta), bush.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (bush.position.x <= (-1 * bush.contentSize.width)) {
-            bush.position = ccp(bush.position.x +
-                                2 * bush.contentSize.width, bush.position.y);
-        }
-    }
-    
-    // move and loop the clouds
-    for (CCNode *cloud in _clouds) {
-        // move the cloud
-        cloud.position = ccp(cloud.position.x -
-                             (character.physicsBody.velocity.x * delta), cloud.position.y);
-        
-        // if the left corner is one complete width off the screen,
-        // move it to the right
-        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
-            cloud.position = ccp(cloud.position.x +
-                                 2 * cloud.contentSize.width, cloud.position.y);
-        }
-    }
-    
-    
+       
    
     
     
