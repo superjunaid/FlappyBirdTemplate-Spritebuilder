c901f774eb79aea0fc42522973286ddbf29cbc94
Syed Junaid Ahmed <syedjahmed@usdoj.gov>
2015-02-09 15:55:11 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -59,12 +59,12 @@
 - (void)didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
     
+    _parallaxBackground = [CCParallaxNode node];
+    [_parallaxContainer addChild:_parallaxBackground];
+    
     _grounds = @[_ground1, _ground2];
     _clouds = @[_cloud1, _cloud2];
     _bushes = @[_bush1, _bush2];
-    
-    _parallaxBackground = [CCParallaxNode node];
-    [_parallaxContainer addChild:_parallaxBackground];
     
     //Note that the bush ratio is larger than the cloud
     _bushParallaxRatio = ccp(0.9, 1);
